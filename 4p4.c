37b65ce1722f961b8bbc7d85b72272793a01131d
dhanushrij44 <dhanushrij44@gmail.com>
2026-02-11 10:59:49 +0530

    Add files via upload

---
+++ 4p4.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+int main() { 
+    int a, b, c, d, e, f; 
+    
+    scanf("%d %d", &a, &b); 
+    printf("Quotient=%d Remainder=%d\n", a / b, a % b);
+    scanf("%d %d", &c, &d); 
+    printf("Quotient=%d Remainder=%d\n", c / d, c % d);
+    scanf("%d %d", &e, &f); 
+    printf("Quotient=%d Remainder=%d", e / f, e % f);
+   
+    
+    
+   
+    return 0; 
+    
+}
