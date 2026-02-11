29e6da821e90815dfc1b05985f70aeaa1a2c9c7b
dhanushrij44 <dhanushrij44@gmail.com>
2026-02-11 10:46:43 +0530

    Add files via upload

---
+++ 4p3.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int main() { 
+    int a, b, c, d, e, f; 
+    int product1, product2, product3; 
+    scanf("%d %d", &a, &b); 
+    scanf("%d %d", &c, &d); 
+    scanf("%d %d", &e, &f); 
+    product1 = a * b;
+    product2 = c * d; 
+    product3 = e * f; 
+    printf("%d\n", product1);
+    printf("%d\n", product2); 
+    printf("%d", product3);
+    return 0; 
+    
+}
