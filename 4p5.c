5dd9c741d8e98f9b2603c51cd6b8dd2cdbc024e9
dhanushrij44 <dhanushrij44@gmail.com>
2026-02-11 11:15:39 +0530

    Add files via upload

---
+++ 4p5.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+int main() { 
+    int a, b, c; 
+    int e, f, g; 
+    int h, i, j;
+    int sum1, sum2, sum3;
+    
+    scanf("%d %d %d" , &a, &b, &c);
+     sum1= a + b + c; 
+    printf("%d" , sum1);
+    
+    scanf("%d %d %d" , &e, &f, &g); 
+     sum2 = e + f + g; 
+     printf("%d" , sum2);
+     
+    scanf("%d %d %d" , &h, &i, &j);
+     sum3 = h + i + j; 
+    printf("%d" , sum3);
+
+return 0; 
+    
+}
