5165963258c9e60469583fbdf75b9155dd3c89ba
dhanushrij44 <dhanushrij44@gmail.com>
2026-02-13 11:57:45 +0530

    Add files via upload

---
+++ 23 Days in a Month.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+int main(){
+int month;
+
+scanf("%d",&month);
+
+switch(month){
+    case 1: 
+    printf("January ");
+    printf(" 31 days");
+    break;
+    case 3: 
+    printf("March ");
+    printf(" 31 days");
+    break;
+    case 5:
+    printf("May ");
+    printf(" 31 days");
+    break;
+    case 7:
+    printf("July ");
+    printf(" 31 days");
+    break;
+    case 8:
+    printf("August ");
+    printf(" 31 days");
+    break;
+    case 10:
+    printf("October ");
+    printf(" 31 days");
+    break;
+    case 12:
+    printf("December ");
+    printf(" 31 days");
+    break;
+     
+    
+    
+    
+    case 4: 
+    printf("April ");
+    printf(" 30 days");
+    break;
+    case 6:
+    printf("June ");
+    printf(" 30 days");
+    break;
+    case 9:
+    printf("September ");
+    printf(" 30 days");
+    break;
+    case 11:
+    printf("November ");
+    printf(" 30 days");
+    break;
+    
+    
+    
+    case 2: 
+    printf("February ");
+    printf(" 28 or 29 days");
+    break; 
+    
+    default:
+    printf("Invalid");
+}
+    return 0;
+}
