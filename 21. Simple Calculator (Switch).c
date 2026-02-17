ad0189b2227213848efd6a0015d8372553538ce1
dhanushrij44 <dhanushrij44@gmail.com>
2026-02-17 11:31:29 +0530

    Add files via upload

---
+++ 21. Simple Calculator (Switch).c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+int a,b;
+char op;
+printf("enter : ");
+scanf("%d %c %d",&a,&op,&b);
+
+switch(op){
+    case'+':
+    printf("Result = %d ",a+b);
+    break;
+    
+    case'-':
+    printf("Result = %d ",a-b);
+    break;
+    
+    case'*':
+    printf("Result = %d ",a*b);
+    break;
+    
+    case'/':
+    printf("Result = %d ",a/b);
+    break;
+    
+    default:
+    printf("Invalid");
+}
+    return 0;
+}
